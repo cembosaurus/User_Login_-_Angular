38b8594257052d058b4e07f4ca45d7c72b5147f5
cembosaurus <milos.bencek@gmail.com>
2019-02-13 16:03:55 +1100

    13022019

--- SPA/src/app/_services/error.interceptor.ts
+++ SPA/src/app/_services/error.interceptor.ts
@@ -3,78 +3,75 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-        return next.handle(req).pipe(
-            catchError(err => {
+        return next.handle(req)
+            .pipe(
+                catchError(err => {
 
-                if (err instanceof HttpErrorResponse) {
-
-
-                    // ... UNAUTHORIZED ...
-                    if (err.status === 401) {
-
-                        return throwError(err.statusText);
-
-                    }
+                        if (err instanceof HttpErrorResponse) {
 
 
-                    // ... BAD REQUEST ...
-                    //if (err.status === 400) {
-
-                    //    return throwError(err.error);
-
-                    //}
+                            // ... UNAUTHORIZED ...
+                            if (err.status === 401) {
+                                return throwError(err.statusText);
+                            }
 
 
-                    // ... CUSTOM ...
-                    const custErrFromAPI = err.headers.get('My-Custom-Error');
+                            // ... CUSTOM ...
+                            const customError = err.headers.get('Shit-Happens');
 
-                    if (custErrFromAPI) {
+                            if (customError) {
 
-                        return throwError(custErrFromAPI);
-
-                    }
-
-
-                    // ... MODELSTATE error ... Modelstate error is type of 'object' OR 400 ...
-                    const serverError = err.error.errors;
-                    let modelStateErrors = '';
-
-                    if (serverError && typeof serverError === 'object') {
-
-                        for (const key in serverError) {
-
-                            if (serverError[key]) {
-
-                                modelStateErrors += serverError[key] + '\n';
+                                console.error(customError);
+                                return throwError(customError);
 
                             }
+
+
+                            // ... BAD REQUEST ...
+                            if (err.status === 400) {
+
+                                // ... MODELSTATE (f.e.: empty fields) ...
+                                if (typeof err.error === 'object') {
+
+                                    const serverError = err.error.errors;
+                                    let modelStateErrors = '';
+
+                                    for (const key in serverError) {
+
+                                        if (serverError[key]) {
+    
+                                            modelStateErrors += serverError[key] + '\n';
+    
+                                        }
+    
+                                    }
+    
+                                    return throwError(modelStateErrors || serverError );
+
+                                }
+
+                                // ... f.e.: registering user who already exist in DB ...
+                                return throwError(err.error);
+
+                            }
+
+                            return throwError('Server Error');
+
                         }
-
-                        return throwError(modelStateErrors || serverError);
-
                     }
-
-
-                    return throwError('Server Error');
-
-                }
-            })
-        );
-
+                )
+            );
     }
-
 }
 
-export const ErrorInterceptorProvider = {
 
+export const ErrorInterceptorProvider = {
     provide: HTTP_INTERCEPTORS,
     useClass: ErrorInterceptor,
     multi: true
-
 };
